f20b0b1b829029e065002dd00b8bef78d6af64ff
moreau-it <60264193+moreau-it@users.noreply.github.com>
2025-07-11 10:59:03 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,17 @@
+#!/usr/bin/env bash
 
+# Start virtual display
+Xvfb :0 -screen 0 1024x768x16 &
+sleep 2
+
+# Start lightweight window manager
+fluxbox &
+
+# Start VNC backend
+x11vnc -display :0 -forever -nopw &
+
+# Start noVNC proxy (serves to port 6080)
+websockify --web=/usr/share/novnc/ 6080 localhost:5900 &
+
+# Start OpenCPN GUI
+opencpn
