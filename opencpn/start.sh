5caf8b84d17cc5b5010974f1c4a24a68b950e522
moreau-it <60264193+moreau-it@users.noreply.github.com>
2025-11-30 19:50:25 +0200

    Update start.sh

--- opencpn/start.sh
+++ opencpn/start.sh
@@ -246,9 +246,13 @@ http {
 
       proxy_pass http://127.0.0.1:${INTERNAL_PORT};
       proxy_http_version 1.1;
-      proxy_set_header Host \$host;
-      proxy_set_header X-Real-IP \$remote_addr;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+
+      # Make Kasm think everything is coming from localhost,
+      # just like our working curl tests.
+      proxy_set_header Host 127.0.0.1;
+      proxy_set_header X-Real-IP 127.0.0.1;
+      proxy_set_header X-Forwarded-For 127.0.0.1;
+
       proxy_set_header Upgrade \$http_upgrade;
       proxy_set_header Connection "upgrade";
     }
